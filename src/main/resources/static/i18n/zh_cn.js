8b2589bdd4ddadbd461dfe19b893f6484eb09356
sizhongxia <teamunion@aliyun.com>
2017-06-10 10:37:16 +0800

    国际化文件 - 中文简体

---
+++ src/main/resources/static/i18n/zh_cn.js
@@ -0,0 +1,72 @@
+{
+
+"header" : {
+  "navbar" : {
+    "UPLOAD" : "上传",
+    "new" : {
+      "NEW" : "新建",
+      "PROJECT" : "项目",
+      "TASK" : "任务",
+      "USER" : "用户",
+      "EMAIL" : "邮箱"
+    },
+    "NOTIFICATIONS" : "通知"
+  }
+},
+"aside" : {
+  "nav" : {
+    "HEADER" : "导航",
+    "DASHBOARD" : "仪表盘",
+    "CALENDAR" : "日历",
+    "EMAIL" : "电子邮件",
+    "WIDGETS" : "小部件",
+    "components" : {
+      "COMPONENTS" : "组件",
+      "ui_kits" : {
+        "UI_KITS" : "UI 元件",
+        "BUTTONS" : "按钮",
+        "ICONS" : "图标",
+        "GRID" : "网格",
+        "BOOTSTRAP" : "Bootstrap",
+        "SORTABLE" : "Sortable",
+        "PORTLET" : "Portlet",
+        "TIMELINE" : "时间轴",
+        "VECTOR_MAP" : "Vector Map"
+      },
+      "table" : {
+        "TABLE" : "Table",
+        "TABLE_STATIC" : "Table static",
+        "DATATABLE" : "Datatable",
+        "FOOTABLE" : "Footable"
+      },
+      "form" : {
+        "FORM" : "Form",
+        "FORM_ELEMENTS" : "Form elements",
+        "FORM_VALIDATION" : "Form validation",
+        "FORM_WIZARD" : "Form wizard"
+      },
+      "CHART" : "Chart",
+      "pages" : {
+        "PAGES" : "Pages",
+        "PROFILE" : "Profile",
+        "POST" : "Post",
+        "SEARCH" : "Search",
+        "INVOICE" : "Invoice",
+        "LOCK_SCREEN" : "Lock screen",
+        "SIGNIN" : "Signin",
+        "SIGNUP" : "Signup",
+        "FORGOT_PASSWORD" : "Forgot password",
+        "404" : "404"
+      }
+    },
+    "your_stuff" : {
+      "YOUR_STUFF": "Your Stuff",
+      "PROFILE" : "Profile",
+      "DOCUMENTS" : "Documents"
+    }
+  },
+  "MILESTONE" : "Milestone",
+  "RELEASE" : "Release"
+}
+
+}
